30fe371f6c19d73a8e30de74f57ba6504651fe1d
Agheroo <reauxrenan@outlook.fr>
2024-04-16 17:46:47 +0200

    Fixed issues for path and added docs

--- create_db.py
+++ create_db.py
@@ -5,32 +5,134 @@ import time
 from colorama import Fore, Style
 import sys
 
-"""
-def rm(nom_fichier):
-    try:
-        os.remove(nom_fichier)
-        print(f"Le fichier {nom_fichier} a été supprimé avec succès.")
-    except FileNotFoundError:
-        print(f"Le fichier {nom_fichier} n'existe pas.")
-    except Exception as e:
-        print(f"Une erreur s'est produite lors de la suppression du fichier {nom_fichier}: {e}")
-if(str(sys.argv[1]) != 'sql'):
-    rm('database.db')
-"""
-def file_len(fname):
-    with open(fname , encoding='utf-8') as f:
-        for i, l in enumerate(f, 1):
-            pass
-    return i
+
+def create_tables(db_name: str = "database.db", tables: list =  [""]):
+    """Creates the database
+    ---------------------
+    
+    Note : The file needs to be non-existant before running this script, it will not rewrite over a file with the same name
+
+    -----------------------
+
+    @params
+
+    db_name : The path of the database relative to the script
+    tables : The "CREATE TABLE" list of all the tables of the concerned database
+    """
+
+    #Connection to the DB with sqlite
+    conn = sqlite3.connect(db_name)
+
+    if(len(sys.argv[1])!=0):
+        if(str(sys.argv[1]) != 'sql'):
+            # Génération des tables avec la liste définie avant
+            for table in tables:
+                conn.execute(table)
+        
+    conn.commit()
+    conn.close()
+
+def insert_table(db_path: str = "database.db", attributes : str = "", csv_path : str = "", batch_size : int = 1000, table_name : str = ""):
+    """Reads a csv file to insert its data into a database table with the same name
+    ---------------------
+    
+    Note : The attributes are using the "?, ?, ?, ... , ?" string format for SQL 'executemany' method
+
+    ---------------------
+    @params
+
+    db_path : The path of the database relative to the script
+    attributes : The number of attributes of a table (using the "?, ?, ?, ... , ?" string format for SQL 'executemany' method)
+    csv_path : The path of the csv file that needs to be written in the database
+    batch_size : The size of the batch for continuous requests to prevent memory overflow
+    table_name : The name of the table that needs to be read
+
+    ---------------------
+    @returns
+
+    None
+    """
+    
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+
+    
+    #Pre read the file once to get the number of lines without the header
+    with open(csv_path, 'r', errors = "ignore") as csvfile: 
+        nb_lines = len(csvfile.readlines())
+    nb_lines -= 1
+
+    with open(csv_path, encoding='utf-8', errors = "ignore") as csvfile:
+        r = csv.reader(csvfile, delimiter=',')
+        next(r)  # Skip header of the file (names of columns etc)
+
+        batch = []  # List of the data for the current batch, its size should not go beyond the "batch_size" variable
+
+        #Progress bar when treating the request
+        print(Fore.YELLOW) #Display console in yellow
+        with Bar(f'Processing {table_name} with a batch of {batch_size}...', max=nb_lines) as bar:
+            for (i, row) in enumerate(r):
+                batch.append(row)   #Update the current batch by adding the row
+                
+                if (i + 1) % batch_size == 0: #If batch size reached, push and start another one
+                    cur.executemany(f"INSERT INTO {table_name} VALUES ({attributes})", batch)
+                    batch.clear()
+                
+                bar.next()
+                
+            # Insert remaining values at the end of for loop
+            if batch:
+                cur.executemany(f"INSERT INTO {table_name} VALUES ({attributes})", batch)
+                batch.clear()
+            bar.finish()
+        csvfile.close()
+
+    print(Style.RESET_ALL)
+    conn.commit()
+    conn.close()
+
+def get_attributes(db_folder: str = "imdb-tiny",table_name: str = "") -> str:
+    """Gets all the attributes of a table in a csv file
+    ---------------------
+    
+    Note : The attributes are using the "?, ?, ?, ... , ?" string format for SQL 'executemany' method
+
+    All the CSV files should be in a folder and this script should be out of this folder i.e.  \n
+
+    ... \n
+    | \n
+    create_db.py \n
+    db_folder \n
+        |   table1.csv\n
+        |   table2.csv \n
+        |   ...
+
+    
+    ---------------------
+    @params
+
+    db_folder : The folder in which all the CSV files are located
+    table_name : The name of the current CSV file to get its attributes (the files all need to be in lowercase characters)
+    """ 
+
+    attribute = ""
+    with open(str(db_folder) + "/" + table_name.lower() + ".csv", "r", newline = "", errors="ignore") as file:
+        reader = csv.reader(file)
+        l = len(next(reader))
+        file.close()
+
+    for i in range(l):
+        if(i == l-1):
+            attribute += "?"
+        else:
+            attribute += "?,"
+
+    return attribute
 
 
 
-
-start_time = time.time()
-
-
-# Liste des tables
-listoftable = [
+# Création des tables par SQL (dans une liste python pour faciliter la lecture)
+list_sql_creates = [
     "CREATE TABLE Movies (mid TEXT, titleType TEXT, primaryTitle TEXT, orginialTitle TEXT, isAdult INT, startYear INT, endYear INT, runtimeMinutes INT, PRIMARY KEY(mid))",
     "CREATE TABLE Persons (pid TEXT, primaryName TEXT, birthYear INT, deathYear INT)",
     "CREATE TABLE Genres (mid TEXT, genre TEXT, PRIMARY KEY(mid, genre))",
@@ -44,7 +146,7 @@ listoftable = [
     "CREATE TABLE Characters (mid TEXT, pid TEXT, name TEXT)"
 ]
 
-# Liste des noms de tables
+# Liste des noms de tables nommées précédemment
 table_names = [
     'Movies',
     'Persons',
@@ -59,107 +161,25 @@ table_names = [
     'Writers'
 ]
 
-#Creation des tables dans la db
-def create_tables(db_path):
-    conn = sqlite3.connect(db_path)
-    
-    if(len(sys.argv[1])!=0):
-        if(str(sys.argv[1]) != 'sql'):
-            # Génération des tables
-            for table in listoftable:
-                conn.execute(table)
-        
-    conn.commit()
-    conn.close()
-
-#Insérer les données dans la table
-def insert_table(db_path, attributes, path, batch_size, table_name):
-    
-    conn = sqlite3.connect(db_path)
-    cur = conn.cursor()
-
-    print(Fore.YELLOW)
-    with open(path, encoding='utf-8') as csvfile:
-        r = csv.reader(csvfile, delimiter=',')
-        next(r)  # Skip header
-        batch = []
-        with Bar(f'Processing {table_name} with a batch of {batch_size}...', max=(file_len(path)/batch_size)+1) as bar:
-            for i, row in enumerate(r):
-                batch.append(row)
-                if (i + 1) % batch_size == 0: #If batch size reached, do another batch after pushing
-                    cur.executemany(f"INSERT INTO {table_name} VALUES ({attributes})", batch)
-                    batch.clear()
-                    bar.next()
-            # insertion de la fin des valeurs 
-            if batch:
-                cur.executemany(f"INSERT INTO {table_name} VALUES ({attributes})", batch)
-                batch.clear()
-                bar.next()
-
-
-    print(Style.RESET_ALL)
-    conn.commit()
-    conn.close()
-
-#Fonction pour avoir le nb de colonnes d'une table
-def getTableColumns(db_size,table_name):
-    attribute = ""
-
-    with open("../"+str(db_size) + "/" + table_name.lower() + ".csv", "r", newline = "") as file:
-        reader = csv.reader(file)
-        l = len(next(reader))
-        file.close()
-
-    for i in range(l):
-        if(i == l-1):
-            attribute += "?"
-        else:
-            attribute += "?,"
-
-    return attribute
-
-
-#Création des tables
-create_tables("database.db")
-
-
-#Vérification lors du lancement du script s'il y a le batch_size, sinon met 1000 par défaut
+# Ce script se lance avec un argument dans la console : c'est la taille du batch
+# Vérification lors du lancement du script s'il y a le batch_size, sinon met 1000 par défaut
 if (sys.argv[1] != 0 and str(sys.argv[1]) !='sql'):
     batch_size = int(sys.argv[1])
 elif(len(sys.argv[1]) == 0):
     batch_size = 1000
 
 
+
+start_time = time.time()
+
+# Création des tables
+create_tables("database.db", list_sql_creates)
+
+# Insertion des données dans les tables créées pour la DB 
 # Appel à la fonction insert_table pour chaque fichier
 for table_name in table_names:
-    insert_table("../database.db", getTableColumns("../imdb-medium",table_name), '../imdb-medium/'+table_name.lower()+'.csv', batch_size, table_name)
+    insert_table("database.db", get_attributes("imdb-tiny",table_name), 'imdb-tiny/'+table_name.lower()+'.csv', batch_size, table_name)
 
 
+# Affichage du temps pris pour écrire la DB entière
 print("Task took : " , time.time() - start_time ,"s")
-
-"""
-#REQUETES SQL (question 1 à 5)
-
-def sql_request(db_path,request):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute(request)
-
-    conn.commit()
-    conn.close()
-
-print("Requête question 1 : ")
-sql_request(cur,"SELECT Movies.primaryTitle FROM Movies JOIN Principals ON Movies.mid = Principals.mid JOIN Persons ON Principals.pid = Persons.pid WHERE Persons.primaryName = 'Jean Reno';")
-print("Requête question 2 : ")
-sql_request(cur, "SELECT primaryTitle, averageRating FROM Movies JOIN Genres ON Movies.mid = Genres.mid JOIN Ratings ON Movies.mid = Ratings.mid WHERE startYear = 2000 AND startYear <= 2010 AND titleType = 'movie' AND genre = 'Horror' ORDER BY averageRating DESC limit 3;")
-print("Requête question 3 : ")
-sql_request(cur, "SELECT DISTINCT Persons.primaryName FROM Persons JOIN Writers ON Persons.pid = Writers.pid JOIN Titles ON Writers.mid = Titles.mid WHERE Titles.region != 'ES';")
-print("Requête question 4 : ")
-sql_request(cur, "SELECT Persons.primaryName, COUNT(*) AS nombreDeRoles FROM Persons JOIN Principals ON Persons.pid = Principals.pid GROUP BY Persons.pid ORDER BY nombreDeRoles DESC LIMIT 1;")
-print("Requête question 5 : ")
-sql_request(cur, "")
-"""
-
-
-
-
