7a2936d3f9661d4fcf642c484d5af9182258fe4e
agheroo <reauxrenan@outlook.fr>
2024-04-25 18:58:23 +0200

    Added movie_info creation

--- create_movie_info.py
+++ create_movie_info.py
@@ -1,185 +1,188 @@
+import time
 import pymongo
+from progress.bar import Bar
+from colorama import Fore, Style
 
+client = pymongo.MongoClient('127.0.0.1', 27017)
+db = client["mongo-db"]
 
-def create_movie_info(mongodb_name : str = "mongo-db", collection_name : str = "Movie_info", pipeline : list =  [{}], batch_size : int = 1000) -> None:
-    """Creates a collection for all the useful informations about a movie
+def join_ratings_movie(movie : dict = {}) -> dict:
+    """#### Join the ratings of a given movie to the dict of the movie
     ---------------------
     
-    Note : The client is created with the default localhost/port usage for mongodb
-    
-    The pipeline is doing several junctions, this operation might take a while to complete.
-
-    The collections are all in the same mongodb database
+    ##### Note :
+    - The client is created with the default localhost/port usage for mongodb
 
     ---------------------
-    @params
-    --
+    #### @params
 
-    mongodb_name : The name of the mongodb database to connect to
+    mongodb_name : The name of the mongodb database relative to this script
+    movie : The movie dict that needs to be joined to its rating
+
+    ---------------------
+    #### @returns
+
+    movie : The new movie dict that's been modified with new attributes from ratings
+    """
+
+    ratings = db["Ratings"].find_one({"mid":movie["mid"]})  # Only find_one since every mid is primary key in Ratings and Movies table
+
+    movie["averageRating"] = ratings["averageRating"]
+    movie["numVotes"] = ratings["numVotes"]
+
+    return movie
+
+def join_genres_movie(movie : dict = {}) -> dict:
+    """#### Adds the genres list of a given movie to the dict of the movie
+    ---------------------
+    
+    ##### Note : 
+    - The client is created with the default localhost/port usage for mongodb
+
+    ---------------------
+    #### @params
+
+    mongodb_name : The name of the mongodb database relative to this script
+    movie : The movie dict that needs to be joined to its genres
+
+    ---------------------
+    #### @returns
+
+    movie : The new movie dict that's been modified with new attributes from genres
+    """
+
+    genres = db["Genres"].find(
+        {"mid" : movie["mid"]},
+        {"_id" : 0, "genre" : 1} # Make the _id hidden and genre visible
+    )
+
+    movie["genres"] = [elem["genre"] for elem in genres]    # Gets a list of all the different genres found for a movie
+    return movie
+    
+def join_casting_movie(movie : dict = {}) -> dict:
+    """#### Adds the casting list (actors) and principals (directors / producers...) of a given movie
+    ---------------------
+    
+    ##### Note :
+    - The client is created with the default localhost/port usage for mongodb
+
+    ---------------------
+    #### @params
+
+    movie : The movie dict that needs to be joined to its principal casting
+
+    ---------------------
+    #### @returns
+
+    movie : The new movie dict that's been modified with new attributes (actors and directors)
+    """
+
+    principals = db["Principals"].find(
+        {"mid" : movie["mid"]},
+        {"_id" : 0, "mid" : 0} # Register all principals of the movie in a list
+    )
+
+    principals_pid = list(map(lambda row: row["pid"],principals.clone()))
+
+
+    persons = list(db["Persons"].find(
+        {"pid": {"$in" : principals_pid}},
+        {"_id":0,"pid":1,"primaryName":1}
+    ))
+
+
+    # Join principals, persons and movies
+    movie["principals"] = [
+        {
+            "pid": principal["pid"],
+            "name": pers["primaryName"],
+            "category": principal["category"],
+            "job": principal["job"]
+        }
+        for principal in principals
+        for pers in persons 
+        if pers["pid"] == principal["pid"]
+    ]
+
+    return movie
+
+def join_movie(movie : dict = {}) -> dict:
+    """#### Does all joins of a given movie adding key informations
+    ---------------------
+    
+    ##### Note : 
+    - Joins "Ratings", "Genres" and "Principals / Persons"
+    - Adds for a movie its averageRating, numVotes, actors, directors, and genres
+    ---------------------
+    #### @params
+
+    movie : The movie dict that needs its new informations to be added
+
+    ---------------------
+    #### @returns
+
+    movie : The new movie dict that's been modified with all new attributes
+    """
+
+    movie = {
+        "mid": movie["mid"],
+        "type": movie["titleType"],
+        "startYear": movie["startYear"],
+        "runtimeMinutes": movie["runtimeMinutes"],
+        "primaryTitle": movie["primaryTitle"]
+    }
+    print("")
+    movie = join_ratings_movie(movie)
+    movie = join_genres_movie(movie)
+    
+    start = time.time()
+    movie = join_casting_movie(movie)
+    print(time.time() - start)
+
+    return movie
+
+
+def create_movie_info(collection_name : str = "Movie_info", batch_size : int = 1000) -> None:
+    """#### Creates a collection for all the useful informations about a movie
+    ---------------------
+    
+    ##### Note :
+    - The client is created with the default localhost/port usage for mongodb
+    - The collections are all in the same mongodb database
+
+    ---------------------
+    #### @params
+
     collection_name : The name wanted for the new collection storing all informations about a movie
-    pipeline : The pipeline command to use the 'aggregate()' method on
     batch_size : The size of the batch for continuous requests to prevent memory overflow
 
     ---------------------
-    @returns
-    --
+    #### @returns
 
     None
     """
+    db.drop_collection(collection_name) # Rewrite new collection for movies
 
-    # Connexion pymongo à mongoDB
-    client = pymongo.MongoClient('127.0.0.1', 27017)
-    db = client[mongodb_name]
-    movie_info = db[collection_name]
+    movies = db["Movies"].find({},{"_id":0})
+    nb_movies = db["Movies"].count_documents({})    # Get all rows for movies
 
-    movie = db["Movies"]
-    
-    batch_full = True   # Boolean indicating if we should keep doing other batches
-    i=0 # Index for doing batches
-    while(batch_full):
-        pipeline_pagination = pipeline + [  # Add to the current pipeline a way to skip already written data (respectfully to the batch_size)
-            {"$skip": batch_size*i},
-            {"$limit":batch_size}
-        ]
-
-        result = movie.aggregate(pipeline_pagination)  # Read the 1000 (or less) lines to insert its data
-        batch = list(result)
-        if(batch != []):
-            movie_info.insert_many(batch)   # Write previously read informations in the collection
-
-        if(len(batch) < batch_size):    # Checking if the batch was to its full size, if not then it must be less so we don't need to keep looping
-            batch_full = False
+    batch = []
+    i=0
+    print(Fore.YELLOW)
+    with Bar(f'Processing with a batch of {batch_size}...', max=nb_movies) as bar:  # Progress bar to track progress of data insertion
+        for movie in movies:
+            batch.append(join_movie(movie))
+            i+=1
+            if((i+1) % batch_size == 0):
+                db[collection_name].insert_many(batch)
+                batch.clear()
+            bar.next()
         
-        print("batch "+ str(i))
-        i+=1    # Incrementing this index to skip many through data with the batch_size
+        if(batch):  # If there is remaining data but not enough to complete the batch, insert them
+            db[collection_name].insert_many(batch)
+            bar.finish()
+    print(Style.RESET_ALL)
 
-    # Terminating client connection
-    client.close()
-    
-
-
-# Definition de la pipeline utilisée (avec jointures et même structure qu'éxpliqué dans le compte-rendu)
-pipeline = [
-   
-    # Jointure de movies et principals (mid)
-    {
-        "$lookup":
-        {
-            "from": "Principals",
-            "localField": "mid",
-            "foreignField": "mid",
-            "as": "principals"
-        }
-    },
-    {
-        "$unwind": "$principals"
-    },
-    # Recherche des job "actor" et "director" lors de la jointure
-    {
-        "$match":
-        {
-            "principals.category": { "$in": ["actor", "director"] }
-        }
-    },
-    
-    # Jointure entre principals et persons (pid)
-    {
-        "$lookup":
-        {
-            "from": "Persons",
-            "localField": "principals.pid",
-            "foreignField": "pid",
-            "as": "casting"
-        }
-    },
-    {
-        "$unwind": "$casting"
-    },
-    
-    # Jointure de movies et ratings
-    {
-        "$lookup":
-        {
-            "from": "Ratings",
-            "localField": "mid",
-            "foreignField": "mid",
-            "as": "rating"
-        }
-    },
-    {
-        "$unwind": "$rating"
-    },
-
-    # Jointure de movies et genres (mid)
-    {
-        "$lookup":
-        {
-            "from": "Genres",
-            "localField": "mid",
-            "foreignField": "mid",
-            "as": "genre"
-        }
-    },
-    {
-        "$unwind": "$genre"
-    },
-
-
-
-
-    
-    {
-        "$group":{
-            "_id": "$movie.mid",
-            "mid" : {"$last" : "$movie.mid"},
-            "primaryTitle": { "$last" : "$movie.primaryTitle" },
-            "runtimeMinutes" : {"$last": "$movies.runtimeMinutes"},
-            "startYear": { "$last" : "$movie.startYear" },
-            "averageRating": { "$last" : "$rating.averageRating" },
-            "numVotes":{"$last" : "$rating.numVotes"},
-            "genre": { "$addToSet": "$genre.genre" },
-            "actors":{ "$addToSet": {    # Rajout des acteurs dans la liste du casting
-                    "$cond": [{ "$eq": [ "$principals.category", "actor" ] }, "$casting.primaryName", None] # Vérification que le "job" est bien "actor"
-                }
-            }, 
-            "directors": { "$addToSet": { # Rajout des directeurs dans la liste du casting
-                    "$cond": [{ "$eq": [ "$principals.category", "director" ] }, "$casting.primaryName", None] # Vérification que le "job" est bien "director"
-                }
-            }
-
-        }
-    },
-    
-    # Création de l'objet à charger en JSON
-    {
-        "$project": {
-            "_id": 0,    # 0 Signifie que _id ne sera pas dans le résultat de la requête
-            "mid" : 1,
-            "primaryTitle":1,
-            "runtimeMinutes":1,
-            "startYear":1,
-            "averageRating":1,
-            "numVotes":1,
-            "genre":1,
-            "actors": {
-                "$filter": {
-                    "input": "$actors",
-                    "as": "actor",
-                    "cond": { "$ne": [ "$$actor", None ] }
-                }
-            },
-            "directors": {
-                "$filter": {
-                    "input": "$directors",
-                    "as": "director",
-                    "cond": { "$ne": [ "$$director", None ] }
-                }
-            }
-        }
-    }
-
-
-]
-
-
-create_movie_info("mongo-db", "Movie_info",pipeline,1000)
+  
+create_movie_info("mongo-db", "Movie_info",1000)
+client.close()
